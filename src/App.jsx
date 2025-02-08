0a7ebd7185f10db27b227cd1302ab04e27f64b44
Gabriel Williams <gabrielskwilliams@gmail.com>
2025-02-07 18:33:52 -0800

    css cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
 import LightweightAPI from './components/LightweightAPI.jsx';
 import LightweightLocal from './components/LightweightLocal.jsx';
-import './styles/App.css'
-import './styles/index.css'
 import './styles/layout.css'
 
 function App() {
